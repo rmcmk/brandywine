8dd10563cb758e59f927b4a669502396ce3a238a
atomicint <ryley.kimmel@live.com>
2016-01-17 03:14:23 -0500

    Document, removed dummy command.

--- src/main/java/me/ryleykimmel/brandywine/game/command/CommandEvent.java
+++ src/main/java/me/ryleykimmel/brandywine/game/command/CommandEvent.java
@@ -3,21 +3,50 @@ package me.ryleykimmel.brandywine.game.command;
 import me.ryleykimmel.brandywine.game.event.PlayerEvent;
 import me.ryleykimmel.brandywine.game.model.player.Player;
 
+/**
+ * A PlayerEvent which is listened for when a {@link Player} inputs a command.
+ * 
+ * @author Ryley Kimmel <ryley.kimmel@live.com>
+ */
 public final class CommandEvent extends PlayerEvent {
 
+  /**
+   * The name of the command.
+   */
   private final String name;
+
+  /**
+   * The command's arguments.
+   */
   private final CommandArguments arguments;
 
+  /**
+   * Constructs a new {@link CommandEvent} with the specified Player, name and CommandArguments.
+   * 
+   * @param player The Player who is listening for commands.
+   * @param name The name of the command.
+   * @param arguments The command's arguments.
+   */
   public CommandEvent(Player player, String name, CommandArguments arguments) {
     super(player);
     this.name = name;
     this.arguments = arguments;
   }
 
+  /**
+   * Gets the name of the command.
+   * 
+   * @return The name of the command.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Gets the arguments for the command.
+   * 
+   * @return The arguments for the command.
+   */
   public CommandArguments getArguments() {
     return arguments;
   }
--- src/main/java/me/ryleykimmel/brandywine/game/command/CommandEventListener.java
+++ src/main/java/me/ryleykimmel/brandywine/game/command/CommandEventListener.java
@@ -1,26 +1,48 @@
 package me.ryleykimmel.brandywine.game.command;
 
 import me.ryleykimmel.brandywine.game.event.EventListener;
+import me.ryleykimmel.brandywine.game.model.Position;
+import me.ryleykimmel.brandywine.game.model.player.Player;
+import me.ryleykimmel.brandywine.network.msg.impl.ServerChatMessage;
 
+/**
+ * Listens for CommandEvent's.
+ * 
+ * @author Ryley Kimmel <ryley.kimmel@live.com>
+ */
 public final class CommandEventListener implements EventListener<CommandEvent> {
 
   @Override
   public void handle(CommandEvent event) {
+    Player player = event.getPlayer();
     CommandArguments args = event.getArguments();
 
     switch (event.getName()) {
-      case "ban":
-        if (!args.remainingExact(3)) {
-          // notify too many/too little arguments
+      case "pos":
+        player.write(new ServerChatMessage("You are standing at: %s", player.getPosition()));
+        return;
+
+      case "tele-to":
+        if (!args.remainingExact(2)) {
+          player.write(new ServerChatMessage(
+              "There are 2 required arguments: ::tele-to [x, y, optional-height]"));
           return;
         }
 
-        String name = args.getNext();
-        int duration = args.getNextInteger();
-        String reason = args.getNext();
-        // ...
-        break;
+        try {
+          int x = args.getNextInteger();
+          int y = args.getNextInteger();
+          int height = args.remaining(1) ? args.getNextInteger() : player.getPosition().getHeight();
+
+          player.teleport(new Position(x, y, height));
+        } catch (NumberFormatException cause) {
+          player
+              .write(new ServerChatMessage("The arguments for this command may only be numeric."));
+        }
+
+        return;
     }
+
   }
 
 }
