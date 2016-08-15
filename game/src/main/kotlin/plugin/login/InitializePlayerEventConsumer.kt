6f2f8cc9a5579b8c6d13a62c481724c955f57bad
Ryley Kimmel <ryley.kimmel@live.com>
2016-08-14 22:58:42 -0400

    Move IsaacRandomPair construction out of plugin

--- game/src/main/kotlin/plugin/login/InitializePlayerEventConsumer.kt
+++ game/src/main/kotlin/plugin/login/InitializePlayerEventConsumer.kt
@@ -3,7 +3,6 @@ package plugin.login
 import me.ryleykimmel.brandywine.game.event.Consumes
 import me.ryleykimmel.brandywine.game.event.EventConsumer
 import me.ryleykimmel.brandywine.game.model.player.InitializePlayerEvent
-import me.ryleykimmel.brandywine.game.model.player.PlayerCredentials
 import me.ryleykimmel.brandywine.game.model.skill.LevelUpSkillListener
 import me.ryleykimmel.brandywine.game.model.skill.SynchronizationSkillListener
 import me.ryleykimmel.brandywine.game.msg.InitializePlayerMessage
@@ -11,7 +10,6 @@ import me.ryleykimmel.brandywine.game.msg.RebuildRegionMessage
 import me.ryleykimmel.brandywine.network.frame.codec.CipheredFrameCodec
 import me.ryleykimmel.brandywine.network.frame.codec.FrameCodec
 import me.ryleykimmel.brandywine.network.frame.codec.FrameMessageCodec
-import me.ryleykimmel.brandywine.network.isaac.IsaacRandom
 import me.ryleykimmel.brandywine.network.isaac.IsaacRandomPair
 import plugin.message
 import plugin.message.MessageRegistrar
@@ -23,7 +21,7 @@ class InitializePlayerEventConsumer : EventConsumer<InitializePlayerEvent> {
         with(event.player) {
             // TODO: Better solution for this, context switching looks terrible :-(
             val pipeline = session.channel.pipeline()
-            pipeline.replace(FrameCodec::class.java, "ciphered_frame_codec", CipheredFrameCodec(session, MessageRegistrar.gameMetadata, credentials.getRandomPair()))
+            pipeline.replace(FrameCodec::class.java, "ciphered_frame_codec", CipheredFrameCodec(session, MessageRegistrar.gameMetadata, IsaacRandomPair.fromSeed(credentials.sessionKeys)))
             pipeline.replace(FrameMessageCodec::class.java, "message_codec", FrameMessageCodec(MessageRegistrar.gameMetadata))
 
             lastKnownRegion = position
@@ -38,16 +36,6 @@ class InitializePlayerEventConsumer : EventConsumer<InitializePlayerEvent> {
 
             message("Welcome to %s.", "Brandywine") // TODO: Add config for this
         }
-    }
-
-    fun PlayerCredentials.getRandomPair(): IsaacRandomPair {
-        val copy = sessionKeys.clone()
-
-        val decodingRandom = IsaacRandom(copy)
-        copy.forEachIndexed { index, i -> copy[index] += 50 }
-        val encodingRandom = IsaacRandom(copy)
-
-        return IsaacRandomPair(encodingRandom, decodingRandom)
     }
 
 }
--- network/src/main/java/me/ryleykimmel/brandywine/network/isaac/IsaacRandomPair.java
+++ network/src/main/java/me/ryleykimmel/brandywine/network/isaac/IsaacRandomPair.java
@@ -1,5 +1,7 @@
 package me.ryleykimmel.brandywine.network.isaac;
 
+import java.util.Arrays;
+
 /**
  * A pair of two {@link IsaacRandom} random number generators used as a stream cipher. One takes the
  * role of an encoder for this endpoint, the other takes the role of a decoder for this endpoint.
@@ -17,12 +19,28 @@ public final class IsaacRandomPair {
   private final IsaacRandom encodingRandom;
 
   /**
+   * Constructs a new IsaacRandomPair from the specified seed.
+   *
+   * @param seed The seed to construct the IsaacRandomPair from.
+   * @return A new IsaacRandomPair, never {@code null}.
+   */
+  public static IsaacRandomPair fromSeed(int[] seed) {
+    int[] copy = seed.clone();
+
+    IsaacRandom decodingRandom = new IsaacRandom(copy);
+    Arrays.setAll(copy, index -> copy[index] += 50);
+    IsaacRandom encodingRandom = new IsaacRandom(copy);
+
+    return new IsaacRandomPair(encodingRandom, decodingRandom);
+  }
+
+  /**
    * Constructs a new {@link IsaacRandomPair} with the specified encoding and decoding random.
    *
    * @param encodingRandom The IsaacRandom number generator used for encoding.
    * @param decodingRandom The IsaacRandom number generator used for decoding.
    */
-  public IsaacRandomPair(IsaacRandom encodingRandom, IsaacRandom decodingRandom) {
+  private IsaacRandomPair(IsaacRandom encodingRandom, IsaacRandom decodingRandom) {
     this.encodingRandom = encodingRandom;
     this.decodingRandom = decodingRandom;
   }
